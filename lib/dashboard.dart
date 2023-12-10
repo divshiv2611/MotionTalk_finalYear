7a46a5891ad0b019869aa35a359d98884439a019
Asus <divyanshsinghds2611@gmail.com>
2023-12-10 17:58:50 +0530

    dasboard minor

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -10,6 +10,66 @@ class MyDashboard extends StatefulWidget {
 class _MyDashboardState extends State<MyDashboard> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+            alignment: Alignment.bottomCenter,
+            height: 170,
+            decoration: BoxDecoration(color: Color(0xff006491),),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Motion Talk', style:
+                      TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),),
+                    Text('Dashboard', style:
+                    TextStyle(
+                      fontSize: 17,
+                      color: Colors.white,
+                    ),),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.black.withOpacity(.1),
+                      ),
+                      child: IconButton(
+                        icon: Icon(Icons.chat,size: 28,color: Colors.white,),
+                        onPressed: (){},
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.black.withOpacity(.1),
+                      ),
+                      child: IconButton(
+                        icon: Icon(Icons.person,size: 28,color: Colors.white,),
+                        onPressed: (){},
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
