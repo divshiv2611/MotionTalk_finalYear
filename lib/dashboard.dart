5ad0cc3f62590154f84195b4187fd5ec58faea9c
Asus <divyanshsinghds2611@gmail.com>
2023-12-11 00:46:56 +0530

    dashboard completed ui

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class MyDashboard extends StatefulWidget {
@@ -10,6 +8,7 @@ class MyDashboard extends StatefulWidget {
 }
 
 class _MyDashboardState extends State<MyDashboard> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +18,9 @@ class _MyDashboardState extends State<MyDashboard> {
             padding: EdgeInsets.symmetric(vertical: 25, horizontal: 25),
             alignment: Alignment.bottomCenter,
             height: 170,
-            decoration: BoxDecoration(color: Color(0xff006491),),
+            decoration: BoxDecoration(
+              color: Color(0xff006491),
+            ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -27,17 +28,21 @@ class _MyDashboardState extends State<MyDashboard> {
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Motion Talk', style:
-                      TextStyle(
+                    Text(
+                      'Motion Talk',
+                      style: TextStyle(
                         fontSize: 26,
                         fontWeight: FontWeight.bold,
                         color: Colors.white,
-                      ),),
-                    Text('Dashboard', style:
-                    TextStyle(
-                      fontSize: 17,
-                      color: Colors.white,
-                    ),),
+                      ),
+                    ),
+                    Text(
+                      'Dashboard',
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.white,
+                      ),
+                    ),
                   ],
                 ),
                 Row(
@@ -48,8 +53,12 @@ class _MyDashboardState extends State<MyDashboard> {
                         color: Colors.black.withOpacity(.1),
                       ),
                       child: IconButton(
-                        icon: Icon(Icons.chat,size: 28,color: Colors.white,),
-                        onPressed: (){},
+                        icon: Icon(
+                          Icons.chat,
+                          size: 28,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
                       ),
                     ),
                     SizedBox(
@@ -61,8 +70,12 @@ class _MyDashboardState extends State<MyDashboard> {
                         color: Colors.black.withOpacity(.1),
                       ),
                       child: IconButton(
-                        icon: Icon(Icons.person,size: 28,color: Colors.white,),
-                        onPressed: (){},
+                        icon: Icon(
+                          Icons.person,
+                          size: 28,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
                       ),
                     )
                   ],
@@ -70,7 +83,9 @@ class _MyDashboardState extends State<MyDashboard> {
               ],
             ),
           ),
-          const SizedBox(height: 25,),
+          const SizedBox(
+            height: 25,
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 25),
             child: Row(
@@ -78,22 +93,21 @@ class _MyDashboardState extends State<MyDashboard> {
               children: [
                 RichText(
                   text: const TextSpan(
-                    text: "Storage",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    children: [
-                      TextSpan(
-                        text: "9.1/10 GB",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w300,
-                          fontSize: 16,
-                        )
-                      )
-                    ]),
+                      text: "Storage",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      children: [
+                        TextSpan(
+                            text: "9.1/10 GB",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w300,
+                              fontSize: 16,
+                            ))
+                      ]),
                 ),
                 const Text(
                   "Upgrade",
@@ -124,8 +138,11 @@ class _MyDashboardState extends State<MyDashboard> {
                     const SizedBox(
                       height: 8,
                     ),
-                    const Text("Sources",
-                    style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold),)
+                    const Text(
+                      "Sources",
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    )
                   ],
                 ),
                 Column(
@@ -139,8 +156,11 @@ class _MyDashboardState extends State<MyDashboard> {
                     const SizedBox(
                       height: 8,
                     ),
-                    const Text("Docs",
-                      style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold),)
+                    const Text(
+                      "Docs",
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    )
                   ],
                 ),
                 Column(
@@ -154,8 +174,11 @@ class _MyDashboardState extends State<MyDashboard> {
                     const SizedBox(
                       height: 8,
                     ),
-                    const Text("Images",
-                      style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold),)
+                    const Text(
+                      "Images",
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    )
                   ],
                 ),
                 Column(
@@ -169,48 +192,219 @@ class _MyDashboardState extends State<MyDashboard> {
                     const SizedBox(
                       height: 8,
                     ),
-                    const Text("Left",
-                      style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold),)
+                    const Text(
+                      "Left",
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    )
                   ],
                 )
               ],
             ),
           ),
-          const SizedBox(height: 15,),
+          const SizedBox(
+            height: 15,
+          ),
           const Divider(
             height: 20,
           ),
-          Expanded(child: ListView(
+          Expanded(
+              child: ListView(
             padding: EdgeInsets.all(25),
             children: [
-              const Text("Catch Up",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),),
-              SizedBox(height: 15,),
-
-              Row(children: [
-                Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                      color: Colors.grey.shade200,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      padding: EdgeInsets.all(38),
-                      height: 110,
-                      child:
-                      Image.asset('assets/img.png',
-                      ),
-                      
-                    )
-                  ],
-                )
-              ],)
+              const Text(
+                "Catch Up",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: EdgeInsets.all(38),
+                          height: 100,
+                          child: Image.asset(
+                            'assets/img.png',
+                          )),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Expert Advice',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 10,
+                              color: Color(0xff006491),
+                            ),
+                          ))
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: EdgeInsets.all(38),
+                          height: 100,
+                          child: Image.asset(
+                            'assets/faq.png',
+                          )),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'FAQ',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 10,
+                              color: Color(0xff006491),
+                            ),
+                          ))
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: EdgeInsets.all(38),
+                          height: 100,
+                          child: Image.asset(
+                            'assets/about.png',
+                          )),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'About Us',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 10,
+                              color: Color(0xff006491),
+                            ),
+                          ))
+                    ],
+                  )
+                ],
+              ),
+              Divider(
+                height: 60,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Blogs",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Create New',
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff006491)),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              bulidProjectRow('ISL Blog'),
+              bulidProjectRow('ISL Learning Easy Blog'),
+              bulidProjectRow('Sign Language Features'),
+              bulidProjectRow('ASL Learing'),
             ],
           ))
-        ],),
+        ],
+      ),
+      floatingActionButton: Container(
+        decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
+          BoxShadow(color: Colors.white, spreadRadius: 7, blurRadius: 1)
+        ]),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(
+            Icons.camera,
+            color: Color(0xff006491),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          currentIndex: selectedIndex,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.dashboard), label: 'dashboard'),
+            BottomNavigationBarItem(icon: Icon(Icons.add_box), label: 'Post')
+          ]),
+    );
+  }
+
+  Container bulidProjectRow(String folder) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 8),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      height: 65,
+      decoration: BoxDecoration(
+        color: Colors.grey.shade200,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(
+                Icons.image,
+                color: Color(0xff006491),
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Text(
+                folder,
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              )
+            ],
+          ),
+          IconButton(
+              color: Colors.white,
+              onPressed: () {},
+              icon: Icon(
+                Icons.more_vert_rounded,
+                color: Color(0xff006491),
+              )),
+        ],
+      ),
     );
   }
 }
